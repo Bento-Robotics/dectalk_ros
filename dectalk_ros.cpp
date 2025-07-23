cb651864d06c4aa0e8e46dbc6ec72d3b5b17b7ae
snaens <snaens@snaens.net>
2025-07-24 01:42:01 +0200

    initial ROS version
    basic TTS working!
    send it a string and it speaks it

--- dectalk_ros.cpp
+++ dectalk_ros.cpp
@@ -1,33 +1,45 @@
-#include <iostream>
 #include <stdio.h>
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-#define DECTALK_BINARY DECTALK_BINARY_DIR "/say -"
+#define DECTALK_BINARY DECTALK_BINARY_DIR "/say -pre '[:phoneme on]'  -"
 
-class Dectalk_ROS {
+class Dectalk_ROS : public rclcpp::Node {
 public:
-  Dectalk_ROS() {
+  Dectalk_ROS() : Node("dectalk_ros") {
+    // start DECtalk
     dectalk_write_pipe = popen(DECTALK_BINARY, "w");
     if (!dectalk_write_pipe)
-      std::cout << "popen failed!";
+      RCLCPP_ERROR(this->get_logger(), "popen failed!");
+    // turn off buffering - clashes with ROS2
+    setvbuf(dectalk_write_pipe, NULL, _IONBF, 0);
+    // create subscriber with class member function
+    subscriber_string_ = this->create_subscription<std_msgs::msg::String>(
+        "speak", 10,
+        std::bind(&Dectalk_ROS::string_callback, this, std::placeholders::_1));
   }
 
   ~Dectalk_ROS() { pclose(dectalk_write_pipe); }
 
-  void say(std::string text) {
-    fputs(text.append("\n").c_str(), dectalk_write_pipe);
+  void string_callback(std_msgs::msg::String::UniquePtr msg) {
+    if (dectalk_write_pipe) {
+      // append a newline (\n) so DECtalk reads individual messages as sentences
+      if (fputs(msg->data.append("\n").c_str(), dectalk_write_pipe) == EOF)
+        RCLCPP_ERROR(this->get_logger(), "dectalk pipe write failed");
+    } else
+      RCLCPP_ERROR(this->get_logger(), "dectalk pipe closed unexpectedly");
   }
 
 private:
   FILE *dectalk_write_pipe;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber_string_;
 };
 
-int main() {
-  Dectalk_ROS dt = Dectalk_ROS();
-  dt.say("jazz tastic");
-  dt.say("Henlo Wordl");
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Dectalk_ROS>());
+  rclcpp::shutdown();
   return 0;
 }
